29df7f4950c81ff06fcf299a239034e4de848e5e
Andrés Peidro Fernández <drawthedrewpf@gmail.com>
2024-05-27 14:19:32 +0200

    cambio definitivo
    lloro por dentro 2.0

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -6,26 +6,34 @@ import { updateChats, timmer } from "./logic/generator";
 
 /**
  * App component
- * 
+ *
  * @example List: Alfredo, Andrés, Álvaro, Alex (Equipo A)
  * @example Chat: Christian, Laura, Oscar, Leonardo (Equipo B)
  * @example Messages: Luis, forever alone (Equipo C)
  */
 function App() {
-
   const [chats, setChats] = useState([]);
+  const [chat, setChat] = useState([]);
 
   useEffect(() => {
-
-    const interval = setTimeout(() => { setChats(updateChats(chats)) }, timmer);
+    const interval = setTimeout(() => {
+      setChats(updateChats(chats));
+    }, timmer);
 
     return () => clearInterval(interval);
+  });
 
-  })
+  const updateChat = (chat) => {
+    setChat(chat);
+  };
+
+  const updateChats2 = (chats) => {
+    setChat(chats);
+  };
 
   return (
     <div className="container">
-      <List />
+      <List chats={chats} updateChats={updateChats2} updateChat={updateChat} />
       <Chat />
       {/* <Notification /> */}
     </div>
