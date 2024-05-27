4174249ebd85ff16acf9e96a5fbac9dd61a19d2e
TeDeLimon <tysonpopluis@gmail.com>
2024-05-27 10:30:43 +0200

    Generador de chats dinámico
    Actualiza la lista de chats en base a un usuario random y un mensaje random

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Chat from "./components/chat/Chat";
 import List from "./components/list/List";
-import Notification from "./components/notification/Notification";
+import { updateChats, timmer } from "./logic/generator";
+//import Notification from "./components/notification/Notification";
 
 
+/**
+ * App component
+ * 
+ * @example List: Alfredo, Andrés, Álvaro, Alex (Equipo A)
+ * @example Chat: Christian, Laura, Oscar, Leonardo (Equipo B)
+ * @example Messages: Luis, forever alone (Equipo C)
+ */
 const App = () => {
-  /**
-   * List: Alfredo, Andrés, Álvaro, Alex (Equipo A)
-   * Chat: Christian, Laura, Oscar, Leo
-   *
-   */
+
+  const [chats, setChats] = useState([]);
+
+  setTimeout(() => {
+    setChats(updateChats(chats))
+    console.log('chats :>> ', chats);
+  }, timmer);
+
   return (
     <div className="container">
       <List />
